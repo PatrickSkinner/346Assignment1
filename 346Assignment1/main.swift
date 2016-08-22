b42e79f53e244e3600f7ccd2c87e2698e5804679
Patrick Skinner <pskinner@oucs1399.otago.ac.nz>
2016-08-23 10:24:27 +1200

    Initial Commit

---
+++ 346Assignment1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  346Assignment1
+//
+//  Created by Patrick Skinner on 8/23/16.
+//  Copyright © 2016 Patrick Skinner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
