9b0a2097eff572e3876f4f23c4a5d9494c06d071
Patrick Skinner <pskinner@oucs1534.otago.ac.nz>
2016-08-31 14:06:07 +1200

    Updated Matrix class.

--- 346Assignment1/Matrix.swift
+++ 346Assignment1/Matrix.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+extension Float: MatrixData { }
+extension Double: MatrixData { }
+extension Int: MatrixData { }
+
+class Matrix: BasicMatrix, CustomStringConvertible {
+    var rows: Int
+    var columns: Int
+    var transpose: Matrix
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        self.transpose = Matrix.init(rows: columns, columns: rows)
+        
+    }
+    
+    subscript(row: Int, column: Int) -> T{
+        get {
+            return 0;
+        }
+        set {
+            
+        }
+    }
+    
+    func copy() -> Matrix {
+        return self;
+    }
+    
+    var description: String{
+        return "ToString"
+    }
+}
+
+class T: MatrixData {
+}
